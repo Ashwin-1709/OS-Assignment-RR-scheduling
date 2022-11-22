f98db96cce9d5ff958970da518edac3f6245843f
Milind Jain <75107993+jainmilind@users.noreply.github.com>
2022-11-22 23:05:43 +0530

    Update P1.c

--- Assignment-2/P1.c
+++ Assignment-2/P1.c
@@ -1,132 +1,114 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <pthread.h>
 #include <stdbool.h>
-#define MAX 10005
-int N , M , K;
+
+typedef long long ll;
+
+typedef struct {
+    int row_size; // row size
+    int row_from, row_to; // row number
+    char* path; // file path
+    ll* offset; // offset of rows in file
+} thread_params;
+
+int N, M, K;
 // in1.txt -> N x M 
 // in2.txt -> M x K
+ll mat[100][100];
 
-long long **offset_1;
-long long **offset_2;
-long long **matrix_1;
-long long **matrix_2;
-// Global variable to indicate how many rows of file 1 and cols of file 2 have finished being read
-int read_row1 = 0 , read_row2 = 0;
-// Mutex lock to avoid race condition while updating read_row1 , read_col2
-pthread_mutex_t lock1 = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t lock2 = PTHREAD_MUTEX_INITIALIZER;
-typedef struct{
-    bool file_type; // if true read from file 1 else file 2
-    int to , from; // row / col number
-    char *path; // file path
-}param;
+void pre_process_input(int N, int M, ll* offset, FILE* fp) {
+    int bytes = 0;
+    for (int i = 0; i < N; ++i) {
+        char* line;
+        size_t sz = 0;
+        getline(&line, &sz, fp);
+        offset[i] = bytes;
+        int cur_ptr = 0;
+        while (line[cur_ptr] != '\n') {
+            bytes++; cur_ptr++;
+        }
+        bytes++;
+    }
+    fclose(fp);
+}
 
-void *read_file(void *args) {
-    bool flag = ((param*)args)->file_type;
-    int to = ((param*)args)->to , from = ((param*)args)->from;
-    char *file_path = ((param*)args)->path;
-    FILE* fp = fopen(file_path , "r");
-    if(flag) {
-        for(int i = to ; i <= from ; i++) {
-            fseek(fp , offset_1[i][0] , SEEK_SET);
-            for(int j = 0 ; j < M ; j++) {
-                fscanf(fp , "%lld" , &matrix_1[i][j]);
-            }
+void* read_file(void* args) {
+    thread_params* P = (thread_params*)args;
+    int start = P->row_from, end = P->row_to;
+    int M = P->row_size;
+    FILE* fp = fopen(P->path, "r");
+    ll* offset = P->offset;
+
+    for (int i = start; i <= end; ++i) {
+        fseek(fp, offset[i], SEEK_SET);
+        for (int j = 0; j < M; ++j) {
+            fscanf(fp, "%lld", &mat[i][j]);
         }
-        pthread_mutex_lock(&lock1);
-        pthread_mutex_lock(&lock2);
-        read_row1 += (from - to + 1);
-        if(read_row1 == N && read_row2 == M) {
-            // stop timer
-        }
-        pthread_mutex_unlock(&lock1);
-        pthread_mutex_unlock(&lock2);
-    } else {
-        for(int i = to ; i <= from ; i++) {
-            fseek(fp , offset_2[i][0] , SEEK_SET);
-            for(int j = 0 ; j < K ; j++) {
-                fscanf(fp , "%lld" , &matrix_2[i][j]);
-            }
-        }
-        pthread_mutex_lock(&lock1);
-        pthread_mutex_lock(&lock2);
-        read_row2 += (from - to + 1);
-        if(read_row1 == N && read_row2 == M) {
-            // stop timer
-        }
-        pthread_mutex_unlock(&lock2);
-        pthread_mutex_unlock(&lock1);
-        // for(int i = 0 ; i < M ; i++) {
-        //     for(int j = to ; j <= from ; j++) {
-        //         fseek(fp , offset_2[i][j] , SEEK_SET);
-        //         fscanf(fp , "%lld" , &matrix_2[i][j]);
-        //     }
-        // }
-        // pthread_mutex_lock(&lock2);
-        // read_col2 += (from - to + 1);
-        // if(read_col2 == K) {
-        //     // stop timer
-        // }
-        // pthread_mutex_unlock(&lock2);
+    }
+    fclose(fp);
+    pthread_exit(NULL);
+}
+
+void print_matrix(int N, int M) {
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < M; ++j)
+            printf("%lld ", mat[i][j]);
+        printf("\n");
     }
 }
 
-int main(int argc , char* argv[]) {
+int main(int argc, char* argv[]) {
+    if (argc != 7) {
+        printf("Invalid input!!!");
+        exit(0);
+    }
+
     N = atoi(argv[1]);
     M = atoi(argv[2]);
     K = atoi(argv[3]);
-    FILE* fp1 = fopen(argv[4] , "r");
-    FILE* fp2 = fopen(argv[5] , "r");
+    FILE* fp1 = fopen(argv[4], "r");
+    FILE* fp2 = fopen(argv[5], "r");
 
-    offset_1 = malloc(N * sizeof(long long*));
-    matrix_1 = malloc(N * sizeof(long long*));
-    offset_2 = malloc(M * sizeof(long long*));
-    matrix_2 = malloc(M * sizeof(long long*));
-    
-    //Preprocess in1.txt
-    long long bytes = 0;
-    for(int i = 0 ; i < N ; i++) {
-        offset_1[i] = malloc(M * sizeof(long long));
-        char* line;
-        size_t sz = 0;
-        getline(&line , &sz , fp1);
-        int cur_ptr = 0 , pt = 0;
-        offset_1[i][0] = bytes;
-        while(line[cur_ptr] != '\n') {
-            bytes++;
-            cur_ptr++;
-            if(line[cur_ptr] == ' ') 
-                offset_1[i][++pt] = bytes;
-        }
-        bytes++;
-    }
+    ll* offset_1 = malloc(N * sizeof(ll));
+    ll* offset_2 = malloc(M * sizeof(ll));
 
+    // Preprocess in1.txt
+    pre_process_input(N, M, offset_1, fp1);
     // Preprocess in2.txt
-    bytes = 0;
-    for(int i = 0 ; i < M ; i++) {
-        offset_2[i] = malloc(K * sizeof(long long));
-        char* line;
-        size_t sz = 0;
-        getline(&line , &sz , fp2);
-        int cur_ptr = 0 , pt = 0;
-        offset_2[i][0] = bytes;
-        while(line[cur_ptr] != '\n') {
-            bytes++;
-            cur_ptr++;
-            if(line[cur_ptr] == ' ') 
-                offset_2[i][++pt] = bytes;
-        }
-        bytes++;
+    pre_process_input(M, K, offset_2, fp2);
+
+    pthread_t threads[10];
+    for (int i = 0; i < 10; ++i) {
+        thread_params* cur = (thread_params*)(malloc(sizeof(thread_params)));
+        cur->offset = offset_1;
+        cur->path = argv[4];
+        cur->row_from = 5 * i;
+        cur->row_to = 5 * i + 4;
+        cur->row_size = 20;
+        pthread_create(&threads[i], NULL, read_file, (void*)cur);
     }
 
-    long long test;
-    for(int i = 0 ; i < M ; i++) {
-        for(int j = 0 ; j < K ; j++) {
-            fseek(fp2 , offset_2[i][j] , SEEK_SET);
-            fscanf(fp2 , "%lld" , &test);
-            printf("%lld " , test);
-        }
-        printf("\n");
+    for (int i = 0; i < 10; ++i) 
+        pthread_join(threads[i], NULL);
+
+    print_matrix(N, M);
+    /*
+    for (int i = 0; i < N; ++i) {
+        fseek(fp1, offset_1[i], SEEK_SET);
+        char* line;
+        size_t sz = 0;
+        getline(&line, &sz, fp1);
+        printf("%lld, %s", offset_1[i], line);
     }
+
+    for (int i = 0; i < M; ++i) {
+        fseek(fp2, offset_2[i], SEEK_SET);
+        char* line;
+        size_t sz = 0;
+        getline(&line, &sz, fp2);
+        printf("%lld, %s", offset_2[i], line);
+    }
+    */
 }
