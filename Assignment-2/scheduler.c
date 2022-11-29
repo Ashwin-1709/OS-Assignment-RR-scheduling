38e99a027fad6b319396145660fbcd44013e3a2d
Milind Jain <75107993+jainmilind@users.noreply.github.com>
2022-11-29 18:21:38 +0530

    Update scheduler.c

--- Assignment-2/scheduler.c
+++ Assignment-2/scheduler.c
@@ -15,29 +15,51 @@
 #define NANO 1e9
 typedef long long ll;
 
-int QUANTA = 2 , N , M , K;
-int blocks = 0;
-bool P1_run = true;
-
+int QUANTA = 2, N, M, K;
+int blocks[2]; // How many quanta each child has executed
 pid_t child[2];
-struct timespec onlyP2Start, onlyP2end;
+bool done[2] = { false , false };
+struct timespec start[2];
+struct timespec end[2];
 
-// void P1_done(int signum) {
-//     P1_run = false;
-//     blocks++; // That Quanta is over
-//     kill(child[0] , SIGCONT);
-//     printf("Turnaround time for P1 : %d\n ms" , blocks * QUANTA);
-//     printf("Waiting time for P1 : %d\n ms" , ((blocks - 1) * QUANTA) / 2);
-//     clock_gettime(CLOCK_REALTIME, &onlyP2Start);
-// }
-
-void round_robin_switch(int turn) {
-    kill(child[turn] , SIGSTOP);
-    kill(child[turn^1] , SIGCONT);
+int pause_child(int turn) {
+    return kill(child[turn], SIGTSTP);
 }
 
-int main(int argc , char* argv[]) {
-    if(argc != 7) {
+int schedule_child(int turn) {
+    return kill(child[turn], SIGCONT);
+}
+
+void killed() {
+    printf("Killed process\n");
+}
+
+bool round_robin_deque(int turn) {
+    int status = pause_child(turn);
+    blocks[turn]++;
+    if (status == -1) {
+        done[turn] = true;
+        printf("Done set to true\n");
+        clock_gettime(CLOCK_REALTIME, &end[turn]);
+        signal(SIGINT, killed);
+        return false;
+    }
+    return true;
+}
+
+bool round_robin_enqueue(int turn) {
+    if (!blocks[turn]) { //  First time being scheduled
+        clock_gettime(CLOCK_REALTIME, &start[turn]);
+    }
+    int status = schedule_child(turn);
+    if (status == -1)
+        return false;
+    return true;
+}
+
+
+int main(int argc, char* argv[]) {
+    if (argc != 7) {
         printf("Invalid input format\n");
         return 0;
     }
@@ -45,35 +67,65 @@ int main(int argc , char* argv[]) {
     M = atoi(argv[2]);
     K = atoi(argv[3]);
 
-    int mat1_id = shmget(1080 , (N * M) * sizeof(ll) , 0666 | IPC_CREAT);
-    int mat2_id = shmget(153 , (K * M) * sizeof(ll) , 0666 | IPC_CREAT);
-    int flag1_id = shmget(1892 , (N) * sizeof(bool) , 0666 | IPC_CREAT);
-    int flag2_id = shmget(2068 , (K) * sizeof(bool) , 0666 | IPC_CREAT);
+    int mat1_id = shmget(1080, (N * M) * sizeof(ll), 0666 | IPC_CREAT);
+    int mat2_id = shmget(153, (K * M) * sizeof(ll), 0666 | IPC_CREAT);
+    int flag1_id = shmget(1892, (N) * sizeof(bool), 0666 | IPC_CREAT);
+    int flag2_id = shmget(2068, (K) * sizeof(bool), 0666 | IPC_CREAT);
 
-    for (int i = 0; i < 7; i++)
-        printf("%s ", argv[i]);
-    // signal(SIGINT , P1_done);
-    child[0] = fork();
+    // Prevents child to become a zombie process
+    signal(SIGCHLD, SIG_IGN);
 
-    if(child[0]) {
-        child[1] = fork();
-        if(child[1]) {
-            while(wait(NULL) > 0);
-        } else {
-            // usleep(10000);
-            // execl("dummy2.out" , NULL);
-             execl("P2.out" , "./P2.out" , argv[1] , argv[2] , argv[3] , argv[1] , argv[6] , NULL);
-        }
-    } else {
-        // execl("dummy1.out" , NULL);
-        execl("P1.out" , "./P1.out" , argv[1] , argv[2] , argv[3] , argv[4] , argv[5] , argv[6] , "12" , NULL);
+
+    switch (child[0] = fork()) {
+        case -1:
+            printf("Error creatring process");
+            break;
+        case 0: //child
+            printf("Paused process 1\n");
+            kill(getpid(), SIGSTOP);
+            printf("Resumed process 1\n");
+            execl("P1.out", "./P1.out", argv[1], argv[2], argv[3], argv[4], argv[5], argv[6], "12", NULL);
+            break;
+
+        default:
+            printf("Inside main\n");
+            switch (child[1] = fork()) {
+                case -1:
+                    printf("Error creatring process\n");
+                    break;
+                case 0:
+                    printf("Paused process 2\n");
+                    kill(getpid(), SIGSTOP);
+                    printf("Resumed process 2\n");
+                    execl("P2.out", "./P2.out", argv[1], argv[2], argv[3], argv[1], argv[6], NULL);
+                    break;
+                default:
+                    break;
+            }
+            break;
     }
 
-    // shmctl(mat1_id , IPC_RMID , 0);
-    // shmctl(mat2_id , IPC_RMID , 0);
-    // shmctl(flag1_id , IPC_RMID , 0);
-    // shmctl(flag2_id , IPC_RMID , 0);
-    // printf("\nerror: %d\n", errno);
+    int turn = 0;
+    int chances = 0;
+    while (!done[0] || !done[1]) {
+        chances++;
+        if (done[turn]) {
+            printf("Inside done\n");
+            turn ^= 1;
+        }
+        round_robin_enqueue(turn);
+        usleep(QUANTA * 1000);
+        round_robin_deque(turn);
+        turn ^= 1;
+    }
+    
 
+    printf("Exited scheduler\n");
+    double time_taken = (end[0].tv_sec - start[0].tv_sec) + (end[0].tv_nsec - start[0].tv_nsec) / NANO;
+    time_taken *= NANO;
+    printf("Time taken for P1 %lf\n", time_taken);
+    time_taken = (end[1].tv_sec - start[1].tv_sec) + (end[1].tv_nsec - start[1].tv_nsec) / NANO;
+    time_taken *= NANO;
+    printf("Time taken for P2 %lf\n", time_taken);
     return 0;
 }
