4acdd8717266bfdfed5253a1af01f7cf1c5ee251
Ashwin Pugalia <98446038+Ashwin-1709@users.noreply.github.com>
2022-11-30 00:30:57 +0530

    Update P2_bench.c

--- Assignment-2/P2_bench.c
+++ Assignment-2/P2_bench.c
@@ -22,42 +22,40 @@ typedef struct {
     uint* cells;
 }thread_params;
 
-void *compute(void *args) {
+void* compute(void* args) {
     thread_params* P = (thread_params*)args;
     int done = 0;
-    while(done < P->sz) {
-        for(int i = 0 ; i < P->sz ; i++) {
-            if(done == P->sz)
-                break;
-            uint x = P->cells[i] / K ;
-            uint y = P->cells[i] - (K * x);
+    bool* vis = malloc(sizeof(bool) * P->sz);
+    for (int i = 0; i < P->sz; ++i)
+        vis[i] = false;
+
+    while (true) {
+        int unvis = 0;
+        for (int i = 0; i < P->sz; ++i) {
+            if (!vis[i])
+                unvis++;
+            
+            int x = P->cells[i] / K;
+            int y = P->cells[i] % K;
             int id = P->cells[i];
-            if(!flag1[x] || !flag2[y]) 
+
+            if (!flag1[x] || !flag2[y] || vis[i])
                 continue;
-            done++;
-            // printf("Computing %u %u\n" , x , y);
-            // Compute dot product
             ll val = 0;
-            for(int j = 0 ; j < M ; j++) 
+            for (int j = 0; j < M; j++)
                 val += mat1[M * x + j] * mat2[M * y + j];
+
             output[x][y] = val;
+            vis[i] = true;
         }
+        if (!unvis)
+            break;
     }
     pthread_exit(NULL);
 }
 
-void print_matrix(int row, int col, ll** mat) {
-    for (int i = 0; i < row; ++i) {
-        for (int j = 0; j < col; ++j)
-            if (j == col - 1)
-                printf("%lld", mat[i][j]);
-            else printf("%lld ", mat[i][j]);
-        printf("\n");
-    }
-}
-
 int main(int argc , char* argv[]) {
-    if (argc != 5) {
+    if (argc != 4) {
         printf("Invalid input!!!\n");
         exit(0);
     }
@@ -68,7 +66,7 @@ int main(int argc , char* argv[]) {
     N = atoi(argv[1]);
     M = atoi(argv[2]);
     K = atoi(argv[3]);
-    FILE* out = fopen(argv[4] , "w+");
+
     output = malloc(sizeof(ll*) * N);
     for(int i = 0 ; i < N ; i++)
         output[i] = malloc(sizeof(ll) * K);
@@ -78,8 +76,6 @@ int main(int argc , char* argv[]) {
     int mat2_id = shmget(153 , (K * M) * sizeof(ll) , 0666 );
     int flag1_id = shmget(1892 , (N) * sizeof(bool) , 0666 );
     int flag2_id = shmget(2068 , (K) * sizeof(bool) , 0666 );
-
-    // printf("%d %d %d %d\n" , mat1_id , mat2_id , flag1_id , flag2_id);
 
     // Attatching ids
     mat1 = shmat(mat1_id , NULL , 0);
@@ -131,17 +127,7 @@ int main(int argc , char* argv[]) {
     shmctl(flag1_id , IPC_RMID , 0);
     shmctl(flag2_id , IPC_RMID , 0);
 
-    for(int i = 0 ; i < N ; ++i) {
-        for(int j = 0 ; j < K ; j++) {
-            fprintf(out , "%lld" , output[i][j]);
-            if(j != K - 1)
-                fprintf(out , " ");
-        }
-        fprintf(out , "\n");
-    }
-
     fclose(csv_ptr);
-    fclose(out);
 
     return 0;
 }
